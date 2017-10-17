ea5ddc290734239dc2d6f29f3b88731a9c4b7f7a
Shiera <Shiera>
2017-10-17 13:32:17 +0300

    added legend, possibility to save fig and removed axsis

--- src/mapDrawtest.py
+++ src/mapDrawtest.py
@@ -1,6 +1,8 @@
 import geopandas as gpd
 import pandas as pd
 import mapMaker
+import matplotlib.pyplot as plt
+
 '''tests the makeMap method'''
 
 #read the testdata and the shapefile into a pandas dataframe and a geopandas geodataframe
@@ -8,4 +10,5 @@ map = gpd.read_file('data/pkSeutu.shp')
 #reads postinumero as an int, tat is a problem when comparing because it drops 00 from beginniing
 info = pd.read_csv('data/testdata.csv',  dtype={'POSTI_ALUE':str})
 
-mapMaker.makeMap(map, info, 'POSTI_ALUE', 'HE_VAKIY')
+mapMaker.makeMap(map, info, 'POSTI_ALUE', 'HE_VAKIY', 'Helsinki väkiluku')
+
--- src/mapMaker.py
+++ src/mapMaker.py
@@ -20,24 +20,44 @@ def _grupeGeoframeToFrame(geoData, data, groupColumnName):
     geoData = geoData.merge(data, on=groupColumnName)
 
 
-def makeMap(map, info, mergeColumn, drawColumn):
+def makeMap(map, info, mergeColumn, drawColumn, title, savePath = None):
     '''takes a geodataframe 'map' and a dataframe 'info' merges them by column by name mergeColumn
-     and plots he map with the data from drawColumn'''
+     and plots he map with the data from drawColumn
+     if savepath is not given the map is printed, othervise it is saved'''
 
     #merge the dataframe info to the geodataframe map
     _grupeGeoframeToFrame(map, info, groupColumnName=mergeColumn)
 
+    # for making colorbar see the max and min values of the data we want to plot
+    colorbarMin = map[drawColumn].min()
+    colorbarMax = map[drawColumn].max()
+
     #plot
-    map.plot(column = drawColumn)
-    plt.show()
+    plottedMap = map.plot(column = drawColumn, cmap = 'Blues', vmin = colorbarMin, vmax = colorbarMax)
+
+    # get the map as a figure
+    fig = plottedMap.get_figure()
+
+    #remove x and y axis from the map
+    plottedMap.set_axis_off()
+
+    #put the title to the map
+    fig.suptitle(title)
+
+    #some internet workaround for making the colorbar for geopandas (from matplotlib, geopandas doesent have its own yet
+    sm = plt.cm.ScalarMappable(cmap='Blues', norm=plt.Normalize(vmin= colorbarMin, vmax=colorbarMax))
+    # fake up the array of the scalar mappable. Urgh...
+    sm._A = []
+    fig.colorbar(sm)
+
+    if (savePath == None):
+        plt.show()
+    else:
+        plt.savefig(savePath)
 
     #TODO
-    #remove axsis
-    #add legend
-    #add title
-    #save as png
-    #configure how bins, colors etc are chsen
-    #draw municipal borders (use kunnat.shp for it
-    #handle postinumero with missing values
+    #draw municipal borders (use kunnat.shp for it)
+    #handle postinumero with missing values - is this needed?
+    #add unit to legend
 
 
